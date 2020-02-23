ee83a5a8b45392e995d3aa70cf478660b5dcb5d8
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-02-22 20:31:33 -0800

    updated API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api", //"https://thawing-mesa-40257.herokuapp.com/api",
+  API_ENDPOINT: "https://thawing-mesa-40257.herokuapp.com/api",
   TOKEN_KEY: "token-memegram-api"
 };
